aa6eecedfeefcdf88f9810913b48f0f497958803
Andrés Felipe López Franco <aflf1@alu.ua.es>
2024-03-05 16:42:31 +0100

    Se modifica la pagina datos personales

--- src/components/profile/ProfileDetails.jsx
+++ src/components/profile/ProfileDetails.jsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Constants from "expo-constants";
 import { Text, View } from "react-native";
+import {firebase} from "../../../database/firebase";
+import { onAuthStateChanged } from "firebase/auth";
 
 const ProfileDetails = () => {
+    const [user, setUser] = useState(null)
+    useEffect(() => {
+        onAuthStateChanged(firebase.auth, (user) => {
+          setUser(user);
+        });
+      }, []);
+      
     return (
         <View style={styles.container}>
             <Text style={styles.text}>Datos Personales</Text>
+            <Text style={styles.text}>{user}</Text>
         </View>
     );
 }
