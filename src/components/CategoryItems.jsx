b90db4453d4d74976b67287aaaff8ea6c67fd2b7
Andrés Felipe López Franco <aflf1@alu.ua.es>
2024-03-28 12:15:50 +0100

    Se añade funcionalidad de añadir al carrito en CategoryItems

--- src/components/CategoryItems.jsx
+++ src/components/CategoryItems.jsx
@@ -8,6 +8,7 @@ import { collection, getDocs, query , where} from "firebase/firestore";
 import firebase from "../../database/firebase";
 import { useDispatch, useSelector } from "react-redux";
 import { addToCart } from "../../redux/CartReducer";
+import Toast from "react-native-root-toast";
 
 const CategoryItems = ({ navigation, route }) => {
   const categoryId = route.params.id;
@@ -34,12 +35,18 @@ const CategoryItems = ({ navigation, route }) => {
   const onSelect = (id) => {
     setSelected(id);
   };
-  const cart = useSelector((state) => state.cart.cart)
   const addItemToCart = () => {
     const item = products.find((item) => selected === item.id)
     dispatch(addToCart(item))
+    Toast.show('Se ha añadido el producto a tu pedido',{
+      duration: Toast.durations.LONG,
+    })
+    goToHome()
   }
   const dispatch = useDispatch();
+  const goToHome = () => {
+    navigation.navigate("Home")
+  }
   return (
     <View style={{ backgroundColor: theme.colors.background, flex: 1 }}>
       <View style={[{ marginTop: Constants.statusBarHeight }, theme.container]}>
