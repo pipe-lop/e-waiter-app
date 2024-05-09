5b73f6b599f63ac9333ea0256562a9fa9fc27a66
Andrés Felipe López Franco <aflf1@alu.ua.es>
2024-05-09 20:37:03 +0200

    Se completa la vista de detalle de un pedido ya pagado

--- src/components/order/OrderDetail.jsx
+++ src/components/order/OrderDetail.jsx
@@ -1,64 +1,208 @@
-import { ActivityIndicator, StyleSheet, Text, View } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import { doc, getDoc } from 'firebase/firestore'
-import firebase from '../../../database/firebase'
-import theme from '../../theme'
+import {
+  ActivityIndicator,
+  Alert,
+  FlatList,
+  Text,
+  View,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import { doc, getDoc } from "firebase/firestore";
+import firebase from "../../../database/firebase";
+import theme from "../../theme";
+import Constants from "expo-constants";
+import SecondaryHeader from "../navigation/SecondaryHeader";
+import OrderItem from "./OrderItem";
+import Fontisto from "@expo/vector-icons/Fontisto";
+import Button from "../formComponents/Button";
+import { useDispatch, useSelector } from "react-redux";
+import { addToCart, cleanCart, overrideCart } from "../../../redux/CartReducer";
+import Toast from "react-native-root-toast";
 
 const OrderDetail = (props) => {
-    const {navigation, route} = props
+  const { navigation, route } = props;
+  const mapStatus = new Map([
+    ["created", "import"],
+    ["inProgress", "clock"],
+    ["done", "checkbox-active"],
+  ]);
+  const cart = useSelector((state) => state.cart.cart);
 
-    const [loading, setLoading] = useState(true)
-    const [order, setOrder] = useState({
-        id:"",
-        orderId: 0,
-        inSite: true,
-        items: [],
-        status: "",
-        totalAmount: 0.0,
-        createdDate: "",
-        updatedDate: ""
-    })
-    
-    useEffect(() => {
-      if(route.params.id != null){
-        getOrderDetail()
-      }
-    }, [])
+  const dispatch = useDispatch();
 
-    const getOrderDetail = async() => {
-        try{
-            const docRef = doc(firebase.db, 'orders', route.params.id)
-            const docSnap = await getDoc(docRef)
+  const [loading, setLoading] = useState(true);
+  const [order, setOrder] = useState({
+    id: "",
+    orderId: 0,
+    inSite: true,
+    items: [],
+    status: "",
+    totalAmount: 0.0,
+    createdDate: "",
+    updatedDate: "",
+  });
 
-            if(docSnap.exists()){
-                setOrder({
-                    id: docSnap.id,
-                    orderId: docSnap.data().orderId,
-                    inSite: docSnap.data().inSite,
-                    items: docSnap.data().items,
-                    status: docSnap.data().status,
-                    totalAmount: docSnap.data().totalAmount,
-                    createdDate: docSnap.data().createdDate,
-                    updatedDate: docSnap.data().updateDate
-                })
-                setLoading(false)
-            }
-        }catch (e) {
-            console.log("Error al obtener el detalle del pedido: ", e)
-        }
+  useEffect(() => {
+    if (route.params.id != null) {
+      getOrderDetail();
     }
-    
+  }, []);
+
+  const getOrderDetail = async () => {
+    try {
+      const docRef = doc(firebase.db, "orders", route.params.id);
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        setOrder({
+          id: docSnap.id,
+          orderId: docSnap.data().orderId,
+          inSite: docSnap.data().inSite,
+          items: docSnap.data().items,
+          status: docSnap.data().status,
+          totalAmount: docSnap.data().totalAmmount,
+          createdDate: docSnap.data().createdDate,
+          updatedDate: docSnap.data().updateDate,
+        });
+        setLoading(false);
+      }
+    } catch (e) {
+      console.log("Error al obtener el detalle del pedido: ", e);
+    }
+  };
+
+  const handleReorder = () => {
+    if (cart.length > 0) {
+      Alert.alert(
+        "Atención",
+        "Se van a sobreecribir los datos del carrito actual",
+        [
+          {
+            text: "Cancelar",
+            onPress: async () =>
+              console.log("Se cancela volver a realizar un pedido antiguo"),
+          },
+          {
+            text: "Aceptar",
+            onPress: () => {
+              dispatch(overrideCart(order.items));
+              Toast.show("Se ha actualizado tu pedido", {
+                duration: Toast.durations.SHORT,
+                position: Toast.positions.CENTER,
+              });
+              navigation.navigate("Home");
+            },
+          },
+        ]
+      );
+    } else {
+      dispatch(overrideCart(order.items));
+      Toast.show("Se ha actualizado tu pedido", {
+        duration: Toast.durations.SHORT,
+        position: Toast.positions.CENTER,
+      });
+      navigation.navigate("Home");
+    }
+  };
+
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        { loading ? (
-          <ActivityIndicator size="large" color={theme.colors.fontGrey} />
-        ) : (
-            <Text>Pedido: {order.orderId}</Text>
-        )}
+    <View style={styles.container}>
+      {loading ? (
+        <ActivityIndicator size="large" color={theme.colors.fontGrey} />
+      ) : (
+        <View style={styles.loaded}>
+          <View style={styles.body}>
+            <SecondaryHeader
+              navigation={navigation}
+              title={"Resumen del pedido"}
+            />
+            <View style={styles.stats}>
+              <View style={[styles.col_stats, styles.col_1_stats]}>
+                <Fontisto
+                  name={
+                    mapStatus.get(order.status) != ""
+                      ? mapStatus.get(order.status)
+                      : "spinner"
+                  }
+                  size={26}
+                />
+                <Text>{order.status}</Text>
+              </View>
+              <View style={[styles.col_stats, styles.col_2_stats]}>
+                <Fontisto name={"prescription"} size={26} />
+                <Text>Pedido: {order.orderId}</Text>
+              </View>
+              <View style={[styles.col_stats, styles.col_3_stats]}>
+                <Fontisto name={"dollar"} size={26} />
+                <Text>{order.totalAmount} €</Text>
+              </View>
+            </View>
+            <View style={styles.items}>
+              <FlatList
+                data={order.items}
+                keyboardShouldPersistTaps={"handled"}
+                renderItem={({ item }) => (
+                  <OrderItem
+                    name={item.nombre}
+                    price={item.precio}
+                    quantity={item.quantity}
+                  />
+                )}
+                keyExtractor={(item) => item.id}
+              />
+            </View>
+          </View>
+          <View style={theme.footer}>
+            <Button
+              title="Volver a realizar pedido"
+              onPress={() => handleReorder()}
+            />
+          </View>
+        </View>
+      )}
     </View>
-  )
-}
+  );
+};
 
-export default OrderDetail
+export default OrderDetail;
 
-const styles = StyleSheet.create({})
+const styles = {
+  container: {
+    marginTop: Constants.statusBarHeight,
+    flex: 1,
+  },
+  stats: {
+    marginTop: 50,
+    flexDirection: "row",
+    width: "100%",
+    paddingHorizontal: 20,
+    alignItems: "space-between",
+    justifyContent: "space-between",
+  },
+  col_stats: {
+    backgroundColor: theme.colors.white,
+    padding: 10,
+    borderRadius: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  col_1_stats: {
+    width: "35%",
+  },
+  col_2_stats: {
+    width: "33%",
+  },
+  col_3_stats: {
+    width: "23%",
+  },
+  items: {
+    marginTop: 20,
+    paddingHorizontal: 20,
+  },
+  loaded: {
+    flex: 1,
+  },
+  body: {
+    flex: 1,
+  },
+};
